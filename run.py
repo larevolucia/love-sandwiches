d24f15220694e1d540c346dec01cbe64f15f2cbf
larevolucia <reis.lucia@gmail.com>
2025-03-09 13:32:49 +0100

    Add get_stock_values function for user feedback on terminal

--- run.py
+++ run.py
@@ -104,6 +104,17 @@ def calculate_stock_data(data):
         new_stock_data.append(round(stock_num))
     return new_stock_data
 
+def get_stock_values(data):
+    """
+    Provide textual feedback to user 
+    on suggested stock for the next market
+    """
+    headings = SHEET.worksheet('stock').row_values(1)
+    stock_dict = {headings[i]: data[i] for i in range(len(headings))}
+    print("Make the following numbers of sandwiches for next market:\n")
+    print(stock_dict)
+    return stock_dict
+    
 def main():
     """
     Run all program functions
@@ -116,6 +127,8 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, 'stock')
+    stock_values = get_stock_values(stock_data)
+    return stock_values
 
 print('Welcome to Love Sandwiches Data Automation')
 main()
